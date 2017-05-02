a46944eab8792ca28290d4d413afa8ba0c899288
klin1007 <klin1007@hotmail.com>
2017-05-02 16:36:49 +0800

    Initial commit

---
+++ app/src/main/java/net/macdidi/helloandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package net.macdidi.helloandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+//import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+        List<String> names = new ArrayList<>();
+        names.add("Simon");
+        names.add("Mary");
+        names.add("John");
+        */
+/*
+        String name = "Simon";
+
+        Date birthdate = new Date();
+
+        BigInteger balance = new BigInteger("100");
+*/
+    }
+}
